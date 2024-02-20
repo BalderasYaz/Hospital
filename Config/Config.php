5ba7cf582258c3df67422dca597f6613fef50354
BalderasYaz <102882071+BalderasYaz@users.noreply.github.com>
2024-02-20 17:42:56 -0600

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -1,7 +1,8 @@
 <?php
-const base_url = "http://localhost:8081/Hospital/";
-const  host = "localhost";
-const  user  = "root";
-const  pass = "";
-const  db = "hospital";
+const base_url = process.env.BASE_URL;
+const host = process.env.DB_HOST;
+const user = process.env.DB_USER;
+const pass = process.env.DB_PASSWORD;
+const db = process.env.DB_NAME;
+
 ?>
