8a2fe02447e0a91a397bf709358b83a25326df5d
BalderasYaz <102882071+BalderasYaz@users.noreply.github.com>
2024-02-20 18:00:10 -0600

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -1,8 +1,7 @@
 <?php
-const base_url = process.env.BASE_URL;
-const host = process.env.DB_HOST;
-const user = process.env.DB_USER;
-const pass = process.env.DB_PASSWORD;
-const db = process.env.DB_NAME;
-
+const base_url = process.env.port;
+const host = process.env.host;
+const user = process.env.username;
+const pass = process.env.password;
+const db = process.env.database;
 ?>
